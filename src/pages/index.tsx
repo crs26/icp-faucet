21680a9f6a1b83c076559903b3614f7ae0d61ee4
Christian Salvadora <christianrsalvadora@gmail.com>
2023-04-13 17:09:26 +0800

    Add forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +14,33 @@ export default function Home() {
           <h1>
             ICP Faucet
           </h1>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Principal ID"
+              aria-label="Principal ID"
+              aria-describedby="basic-addon2"
+            />
+            <Button className='col-1' variant="outline-success" id="button-addon2">
+              Get ICP
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12 text-center'>
+          <h1>
+            Cycles Faucet
+          </h1>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Wallet Canister ID"
+              aria-label="Wallet Canister ID"
+              aria-describedby="basic-addon2"
+            />
+            <Button className='col-1' variant="outline-success" id="button-addon2">
+              Get Cycles
+            </Button>
+          </InputGroup>
         </div>
       </div>
     </main>
