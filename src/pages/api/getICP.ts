824bea698dba11a4553d033b0b17eb3f4ed11c7b
Christian Salvadora <christianrsalvadora@gmail.com>
2023-04-19 14:39:34 +0800

    Fix return error

--- src/pages/api/getICP.ts
+++ src/pages/api/getICP.ts
@@ -27,7 +27,8 @@ export default async function handler(
             console.log(`ICP claimed by ${req.body.accountId}`)
             res.status(201).end()
         } catch (error) {
-            res.status(400).send(error as Error)
+            console.error(error)
+            res.status(400).send({stderr: error} as any)
         }
     } else {
         res.status(400).end()
