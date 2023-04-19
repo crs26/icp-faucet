1ae254f97b4bdc34b691fbbdefb3112977829bf3
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2023-04-19 16:53:12 +0800

    Update title text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>SparkLearn EdTech ICP & Cycles Faucet</title>
+        <title>SparkLearn ICP Faucet</title>
       </Head>
       <Component {...pageProps}/>
     </>
